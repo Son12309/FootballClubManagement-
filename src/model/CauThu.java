ac6ce6e2a9637c48969973211c056d3aea83188d
Son12309 <leminhson0905@gmail.com>
2025-10-21 15:05:00 +0700

    Update CauThu.java

--- src/model/CauThu.java
+++ src/model/CauThu.java
@@ -1,22 +1,22 @@
 package Model;
-import java.time.LocalDate;
 
 public class CauThu {
+    private int maCT;
     private String hoTen;
     private String quocTich;
     private String viTri;
-    private LocalDate ngaySinh;
-    private int giaMua;    
-    private int luong;        
+    private String ngaySinh; 
+    private double giaMua;
+    private double luong;
     private int soAo;
     private int maCLB;
-   
-    public CauThu() {
-    }
-    
 
-    public CauThu(String hoTen, String quocTich, String viTri, LocalDate ngaySinh, 
-                  int giaMua, int luong, int soAo, int maCLB) {
+    // Constructor không tham số
+    public CauThu() {}
+
+    // Constructor đầy đủ
+    public CauThu(int maCT, String hoTen, String quocTich, String viTri, String ngaySinh, double giaMua, double luong, int soAo, int maCLB) {
+        this.maCT = maCT;
         this.hoTen = hoTen;
         this.quocTich = quocTich;
         this.viTri = viTri;
@@ -26,83 +26,92 @@ public class CauThu {
         this.soAo = soAo;
         this.maCLB = maCLB;
     }
-    
-    // Getter và Setter
+
+    // Getter & Setter
+    public int getMaCT() {
+        return maCT;
+    }
+
+    public void setMaCT(int maCT) {
+        this.maCT = maCT;
+    }
+
     public String getHoTen() {
         return hoTen;
     }
-    
+
     public void setHoTen(String hoTen) {
         this.hoTen = hoTen;
     }
-    
+
     public String getQuocTich() {
         return quocTich;
     }
-    
+
     public void setQuocTich(String quocTich) {
         this.quocTich = quocTich;
     }
-    
+
     public String getViTri() {
         return viTri;
     }
-    
+
     public void setViTri(String viTri) {
         this.viTri = viTri;
     }
-    
-    public LocalDate getNgaySinh() {
+
+    public String getNgaySinh() {
         return ngaySinh;
     }
-    
-    public void setNgaySinh(LocalDate ngaySinh) {
+
+    public void setNgaySinh(String ngaySinh) {
         this.ngaySinh = ngaySinh;
     }
-    
-    public int getGiaMua() {
+
+    public double getGiaMua() {
         return giaMua;
     }
-    
-    public void setGiaMua(int giaMua) {
+
+    public void setGiaMua(double giaMua) {
         this.giaMua = giaMua;
     }
-    
-    public int getLuong() {
+
+    public double getLuong() {
         return luong;
     }
-    
-    public void setLuong(int luong) {
+
+    public void setLuong(double luong) {
         this.luong = luong;
     }
-    
+
     public int getSoAo() {
         return soAo;
     }
-    
+
     public void setSoAo(int soAo) {
         this.soAo = soAo;
     }
-    
+
     public int getMaCLB() {
         return maCLB;
     }
-    
+
     public void setMaCLB(int maCLB) {
         this.maCLB = maCLB;
     }
-    
+
     @Override
     public String toString() {
-        return "Cau Thu{" +
-                "Ho Ten:'" + hoTen + '\'' +
-                ", Quoc Tich:'" + quocTich + '\'' +
-                ", Vi Tri:'" + viTri + '\'' +
-                ", Ngay Sinh=" + ngaySinh +
-                ", Gia Mua:" + giaMua +
-                ", Luong:" + luong +
-                ", So Ao:" + soAo +
-                ", Ma CLB:" + maCLB +
+        return "CauThu{" +
+                "MaCT: " + maCT +
+                ", HoTen: '" + hoTen + '\'' +
+                ", QuocTich: '" + quocTich + '\'' +
+                ", ViTri: '" + viTri + '\'' +
+                ", NgaySinh: '" + ngaySinh + '\'' +
+                ", GiaMua: " + giaMua +
+                ", Luong: " + luong +
+                ", SoAo: " + soAo +
+                ", MaCLB: " + maCLB +
                 '}';
     }
 }
