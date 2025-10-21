6c4a26fa832142a62f30675cbad1003b9d734c47
Son12309 <leminhson0905@gmail.com>
2025-10-21 22:09:25 +0700

    Update PhongDoService.java

--- src/service/PhongDoService.java
+++ src/service/PhongDoService.java
@@ -1,1 +1,25 @@
+package service;
+
+import dao.CauThuDAO;
+import dao.CauThuDAO.PlayerPhongDo;
+import java.util.List;
+
+public class PhongDoService {
+    private final CauThuDAO dao = new CauThuDAO();
+
+    public void printTopScorers(int topN) {
+        List<String> top = dao.getTopScorers(topN);
+        top.forEach(System.out::println);
+    }
+
+    public void printTopPerformer() {
+        PlayerPhongDo p = dao.getTopPlayerByPhongDo();
+        if (p == null) System.out.println("No form data available!");
+        else System.out.println("Top performer: " + p);
+    }
+
+    public void printTop5PhongDo() {
+        dao.getTop5PhongDo().forEach(System.out::println);
+    }
+}
 
